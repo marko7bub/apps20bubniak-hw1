b1118310f0e96a190f62786cfa90067b0a8f3337
marko7bub <bubniak@ucu.edu.ua>
2020-11-13 19:34:07 +0200

    Finished HW

--- src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
+++ src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
@@ -1,5 +1,19 @@
 package ua.edu.ucu.tempseries;
 
 public class TempSummaryStatistics {
-    
+    private double avgTemp;
+    private double devTemp;
+    private double maxTemp;
+    private double minTemp;
+
+    public TempSummaryStatistics(double average, double deviation, double maximum, double minimum){
+        avgTemp = average;
+        devTemp = deviation;
+        maxTemp = maximum;
+        minTemp = minimum;
+    }
+    public double[] SummaryArray(){
+        double[] summary = {avgTemp, devTemp, maxTemp, minTemp};
+        return summary;
+    }
 }
--- src/main/java/ua/edu/ucu/tempseries/TemperatureSeriesAnalysis.java
+++ src/main/java/ua/edu/ucu/tempseries/TemperatureSeriesAnalysis.java
@@ -1,52 +1,162 @@
 package ua.edu.ucu.tempseries;
 
+import java.util.InputMismatchException;
+
 public class TemperatureSeriesAnalysis {
-
+    private double[] tempSeries = {};
+    private int lenSeries;
     public TemperatureSeriesAnalysis() {
-
     }
 
     public TemperatureSeriesAnalysis(double[] temperatureSeries) {
-
+        for (double value:temperatureSeries){
+            if (value<-273){
+                throw new InputMismatchException();
+            }
+        }
+        lenSeries = temperatureSeries.length;
+        tempSeries = temperatureSeries;
     }
 
     public double average() {
-        return -1;
+        if (lenSeries == 0) {
+            throw new IllegalArgumentException();
+        }
+        double sumSeries = 0;
+        for (double value:tempSeries) {
+            sumSeries = sumSeries + value;
+        }
+        return sumSeries/lenSeries;
     }
 
     public double deviation() {
-        return 0;
+        if (lenSeries == 0) {
+            throw new IllegalArgumentException();
+        }
+        double numerator = 0;
+        double denominator = lenSeries;
+        for (double value:tempSeries){
+            numerator = numerator + (value - average())*(value - average());
+        }
+        return Math.sqrt(numerator/denominator);
     }
 
     public double min() {
-        return 0;
+        if (lenSeries == 0) {
+            throw new IllegalArgumentException();
+        }
+        double minSeries = tempSeries[0];
+        for (double value:tempSeries){
+            if (value < minSeries){
+                minSeries = value;
+            }
+        }
+        return minSeries;
     }
 
     public double max() {
-        return 0;
+        if (lenSeries == 0) {
+            throw new IllegalArgumentException();
+        }
+        double maxSeries = tempSeries[0];
+        for (double value:tempSeries){
+            if (value > maxSeries){
+                maxSeries = value;
+            }
+        }
+        return maxSeries;
     }
 
     public double findTempClosestToZero() {
-        return 0;
+        if (lenSeries == 0) {
+            throw new IllegalArgumentException();
+        }
+        return findTempClosestToValue(0.0);
     }
 
     public double findTempClosestToValue(double tempValue) {
-        return 0;
+        if (lenSeries == 0) {
+            throw new IllegalArgumentException();
+        }
+        double distSeries = Math.abs(tempSeries[0] - tempValue);
+        double closestSeries = tempSeries[0];
+        for (double value:tempSeries){
+            if (Math.abs(value - tempValue) < distSeries){
+                distSeries = Math.abs(value - tempValue);
+                closestSeries = value;
+            }
+        }
+        return closestSeries;
     }
 
     public double[] findTempsLessThen(double tempValue) {
-        return null;
+        if (lenSeries == 0) {
+            throw new IllegalArgumentException();
+        }
+        int lesserNumber = 0;
+        for (double value:tempSeries){
+            if (value < tempValue){
+                lesserNumber+=1;
+            }
+        }
+        double[] lesserSeries = new double [lesserNumber];
+        int lesserIndex = 0;
+        for (double value:tempSeries){
+            if (value < tempValue){
+                lesserSeries[lesserIndex] = value;
+                lesserIndex+=1;
+            }
+        }
+        return lesserSeries;
     }
 
     public double[] findTempsGreaterThen(double tempValue) {
-        return null;
+        if (lenSeries == 0) {
+            throw new IllegalArgumentException();
+        }
+        int greaterNumber = 0;
+        for (double value:tempSeries){
+            if (value > tempValue){
+                greaterNumber+=1;
+            }
+        }
+        double[] greaterSeries = new double [greaterNumber];
+        int greaterIndex = 0;
+        for (double value:tempSeries){
+            if (value > tempValue){
+                greaterSeries[greaterIndex] = value;
+                greaterIndex+=1;
+            }
+        }
+        return greaterSeries;
     }
 
     public TempSummaryStatistics summaryStatistics() {
-        return null;
+        if (lenSeries == 0) {
+            throw new IllegalArgumentException();
+        }
+        return new TempSummaryStatistics(average(), deviation(), min(), max());
     }
 
-    public int addTemps(double... temps) {
-        return 0;
+    public double addTemps(double... temps) {
+        int expandedLength = lenSeries;
+        while (expandedLength < lenSeries + temps.length){
+            expandedLength = expandedLength*2;
+        }
+        double[] expandedSeries = new double[expandedLength];
+        int expandedIndex = 0;
+        for (double value:tempSeries){
+            expandedSeries[expandedIndex] = value;
+            expandedIndex = expandedIndex + 1;
+        }
+        for (double value:temps){
+            expandedSeries[expandedIndex] = value;
+            expandedIndex = expandedIndex + 1;
+        }
+        double expandedSum = 0;
+        for (double value:expandedSeries){
+            expandedSum = expandedSum + value;
+        }
+        return expandedSum;
     }
 }
